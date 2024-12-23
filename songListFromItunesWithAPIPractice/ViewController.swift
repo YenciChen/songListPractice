23fd71c3062fd840276bdc4c0e10cef3616d0c8b
Yenci <a278862002@gmail.com>
2024-12-23 15:35:44 +0800

    Initial Commit

---
+++ songListFromItunesWithAPIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  songListFromItunesWithAPIPractice
+//
+//  Created by 陳妍慈 on 2024/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
